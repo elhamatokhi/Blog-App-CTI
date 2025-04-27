ea225af530f2f2b71c49fe90f4ad61c71e154bbb
Elhama Tokhi <tokhielaham30@gmail.com>
2025-04-27 16:31:06 +0200

    Set up the app"

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+// import router from "./routes/router.js";
+
+// Express setup
+const app = express();
+const PORT = 3000;
+
+// Middleware Setup
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
+app.set("view engine", "ejs");
+
+// File and Folder Setup
+if (!fs.existsSync("projects.json")) fs.writeFileSync("projects.json", "[]");
+if (!fs.existsSync("uploads")) fs.mkdirSync("uploads");
+
+// Multer setup
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (_, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+// Route Handling
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
